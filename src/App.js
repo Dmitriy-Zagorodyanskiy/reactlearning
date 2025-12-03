31238a23e5d57b6a91c05ce1229e971515a76fb0
dimzag897@gmail.com <dimzag897@gmail.com>
2025-12-03 18:53:32 +0200

    feat: add constructor, components

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import Image from "./components/image"
 import logo from "./img/logo.png"
 
 class App extends React.Component {
-  helpText = "Help Text"
+  constructor (props) {
+    super(props)
+    this.state = {
+      helpText: "Help Text",
+      userData: ""
+    }
+
+    // this.inputClick = this.inputClick.bind(this)
+  }
+
   render() {
     return (<div className='name'>
     <Header title="titleHead" />
-    <h1>{this.helpText}</h1>
-    <input placeholder={this.helpText}
+    <h1>{this.state.helpText}</h1>
+    <h2>{this.state.userData}</h2>
+    <input placeholder={this.state.helpText}
+      value={this.state.userData}
+      onChange={event => this.setState({userData: event.target.value})}
       onClick={this.inputClick} onMouseEnter={this.mouseOver} />
-      <p>{this.helpText == "Help Text" ? "Yes" : "No"}</p>
+      <p>{this.state.helpText == "Help Text" ? "Yes" : "No"}</p>
       <Image image={logo} />
     </div>)
   }
 
-  inputClick() {console.log("Clicked")}
-  mouseOver() {console.log("Mouse Over")}
+  inputClick = () => {
+    this.setState({helpText: "changed"})
+    console.log("Clicked")
+  }
+  mouseOver = () => {console.log("Mouse Over")}
 }
 
 export default App;
