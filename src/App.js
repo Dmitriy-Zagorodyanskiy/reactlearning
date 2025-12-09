7610a6d6cc4f8c144961e15aa7eebee9ded92302
dimzag897@gmail.com <dimzag897@gmail.com>
2025-12-09 11:21:41 +0200

    feat: add userlist

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import React from 'react';
 import Header from './components/header';
-import Image from "./components/image"
-import logo from "./img/logo.png"
+import Users from './components/users';
 
 class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      helpText: "Help Text",
-      userData: ""
-    }
-
-    // this.inputClick = this.inputClick.bind(this)
-  }
-
+  
   render() {
     return (<div className='name'>
-    <Header title="titleHead" />
-    <h1>{this.state.helpText}</h1>
-    <h2>{this.state.userData}</h2>
-    <input placeholder={this.state.helpText}
-      value={this.state.userData}
-      onChange={event => this.setState({userData: event.target.value})}
-      onClick={this.inputClick} onMouseEnter={this.mouseOver} />
-      <p>{this.state.helpText == "Help Text" ? "Yes" : "No"}</p>
-      <Image image={logo} />
+    <Header title="Users list" />
+
+    <main>
+      <Users />
+    </main>
+    <aside></aside>
     </div>)
   }
 
-  inputClick = () => {
-    this.setState({helpText: "changed"})
-    console.log("Clicked")
-  }
-  mouseOver = () => {console.log("Mouse Over")}
 }
 
 export default App;
