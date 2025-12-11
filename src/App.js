ed76a7be8a677a96f30fc705ba84d4bb22ef6fef
dimzag897@gmail.com <dimzag897@gmail.com>
2025-12-11 12:23:35 +0200

    feat: mods

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React from 'react';
 import Header from './components/header';
 import Users from './components/users';
+import AddUser from './components/adduser';
 
 class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state ={
+          users: [
+              {   id: 1,
+          firstname: 'Bob',
+          lastname: 'Marley',
+          bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, voluptas.',
+          age: 40,
+          isHappy: true
+      },
+      {   id: 2,
+          firstname: 'John',
+          lastname: 'Doe',
+          bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, voluptas.',
+          age: 22,
+          isHappy: false
+      }
+          ]
+      }
+      this.addUser = this.addUser.bind(this)
+  }
   
   render() {
     return (<div className='name'>
+
     <Header title="Users list" />
 
     <main>
-      <Users />
+      <Users  users={this.state.users}/>
     </main>
-    <aside></aside>
-    </div>)
+
+    <aside>
+      <AddUser onAdd={this.addUser}/>
+    </aside>
+
+    </div>
+    )
+  }
+
+  addUser(user) {
+    const id = this.state.users.length + 1
+    this.setState({ users: [...this.state.users, {id, ...user}] })
   }
 
 }
